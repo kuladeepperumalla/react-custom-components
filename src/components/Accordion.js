410ff4ca9f6b7cd556cb1f31085f6f0e3a25b1c2
kuldeepperumalla <kuldeepperumalla325@gmail.com>
2023-03-01 11:51:41 -0500

    commit #4 toggle feature

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,17 +1,36 @@
 import { useState } from "react";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
+
 function Accordion({items}){
-    const [expandedIndex, setExpandedIndex] = useState(0);
+    const [expandedIndex, setExpandedIndex] = useState(-1);
+            const handleclick = (nextIndex) => {
+                if(expandedIndex === nextIndex){
+                    setExpandedIndex(-1);
+                }else{
+                    setExpandedIndex(nextIndex);
+                }
+            };
+
     const renderedItems = items.map((item, index)=>{
         const isExpanded = index === expandedIndex;
+        const icon = (
+          <span>{isExpanded ? <GoChevronDown /> : <GoChevronLeft />}</span>
+        );
         return (
-            <div key={item.id}>
-                <div>{item.label}</div>
-                {isExpanded && <div>{item.content}</div>}
+          <div key={item.id}>
+            <div
+              className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer"
+              onClick={() => handleclick(index)}
+            >
+              {item.label}
+              {icon}
             </div>
-            
-        )
+
+            {isExpanded && <div className="border-b p-5">{item.content}</div>}
+          </div>
+        );
     })
-    return <div>{renderedItems}</div>
+    return <div className="border-x border-t rounded">{renderedItems}</div>
 }
 
 export default Accordion;
