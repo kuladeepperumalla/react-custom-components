0359debbedf4979fc0c1c9809012bdb855638dea
kuldeepperumalla <kuldeepperumalla325@gmail.com>
2023-03-13 11:40:21 -0400

    commit #16 custom table headers

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,19 +1,23 @@
-function Table({data}){
+function Table({data, config}){
+    const renderedHeaders = config.map((column)=>{
+        return <th key={column.label}>{column.label}</th>
+    })
     const renderedRows = data.map((fruit)=>{
         return (
-          <tr key={fruit.name}>
-            <td>{fruit.name}</td>
-            <td>{fruit.color}</td>
-            <td>{fruit.score}</td>
+          <tr className="border-b" key={fruit.name}>
+            <td className="p-3">{fruit.name}</td>
+            <td className="p-3">
+              <div className={`p-3 m-2 ${fruit.color}`}></div>
+            </td>
+
+            <td className="p-3">{fruit.score}</td>
           </tr>
         );
     })
-    return <table>
+    return <table className="table-auto border-spacing-2">
         <thead>
-            <tr>
-                <th>Fruit</th>
-                <th>Color</th>
-                <th>Score</th>
+            <tr className="border-b-2">
+                {renderedHeaders}
             </tr>
         </thead>
         <tbody>
--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -4,12 +4,19 @@ function TablePage(){
     const data = [
         {name:'orange', color:'bg-orange-500', score:5},
         {name:'apple', color:'bg-red-500', score:3},
-        {name:'banana', color:'bg-yello-500', score:1},
-        {name:'lime', color:'bg-yello-500', score:4}
+        {name:'banana', color:'bg-yellow-500', score:1},
+        {name:'lime', color:'bg-green-500', score:4}
+    ];
+
+    const config = [
+        {label:'Name'},
+        {label:'Color'},
+        {label:'Score'}
     ]
+
     return (
     <div>
-        <Table data={data}/>
+        <Table data={data} config={config}/>
     </div>
     )
 }
