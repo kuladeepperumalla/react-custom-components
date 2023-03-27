2932fc4ae041e36127569a78ee98ff22b88ae8a4
kuldeepperumalla <kuldeepperumalla325@gmail.com>
2023-03-26 20:02:24 -0400

    commit #25 useReducer

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,31 +1,66 @@
+import produce from "immer";
 import { useReducer } from "react";
 import Button from "../components/Button";
 import Panel from "../components/Panel";
 
+const INCREMENT_COUNT = "increment";
+const SET_VALUE_TO_ADD = "change_value_to_add";
+const DECREMENT_COUNT = "decrement";
+const ADD_VALUE_TO_COUNT = "add_value_to_count";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case INCREMENT_COUNT:
+      state.count = state.count + 1;
+      return;
+    case DECREMENT_COUNT:
+      state.count = state.count - 1;
+      return;
+    case ADD_VALUE_TO_COUNT:
+      state.count = state.count + state.valueToAdd;
+      state.valueToAdd = 0;
+      return;
+    case SET_VALUE_TO_ADD:
+      state.valueToAdd = action.payload;
+      return;
+    default:
+      return;
+  }
+};
+
 function CounterPage({ initialCount }) {
-//   const [count, setCount] = useState(initialCount);
-//   const [valueToAdd, setValueToAdd] = useState(0);
-const [ state, dispatch] = useReducer(reducer, {
+  // const [count, setCount] = useState(initialCount);
+  // const [valueToAdd, setValueToAdd] = useState(0);
+  const [state, dispatch] = useReducer(produce(reducer), {
     count: initialCount,
     valueToAdd: 0,
-})
+  });
+  console.log(state);
 
   const increment = () => {
-    // setCount(count + 1);
+    dispatch({
+      type: INCREMENT_COUNT,
+    });
   };
   const decrement = () => {
-    // setCount(count - 1);
+    dispatch({
+      type: DECREMENT_COUNT,
+    });
   };
   const handleChange = (event) => {
     const value = parseInt(event.target.value) || 0;
 
-    // setValueToAdd(value);
+    dispatch({
+      type: SET_VALUE_TO_ADD,
+      payload: value,
+    });
   };
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    // setCount(count + valueToAdd);
-    // setValueToAdd(0);
+    dispatch({
+      type: ADD_VALUE_TO_COUNT,
+    });
   };
 
   return (
@@ -39,7 +74,7 @@ const [ state, dispatch] = useReducer(reducer, {
       <form onSubmit={handleSubmit}>
         <label>Add a lot!</label>
         <input
-          value={statevalueToAdd || ""}
+          value={state.valueToAdd || ""}
           onChange={handleChange}
           type="number"
           className="p-1 m-3 bg-gray-50 border border-gray-300"
